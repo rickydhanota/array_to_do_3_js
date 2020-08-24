e31dadc244f0dce7574ecdb473b8686974877388
Ricky <rickdhanota@gmail.com>
2020-08-23 18:58:30 -0700

    trying to figure out how to make a negative disappear from the rest of the array

--- to-do-3.js
+++ to-do-3.js
@@ -1,12 +1,21 @@
 //Implement removeNegatives() that accepts an array, removes negative values, and returns the same array (not a copy), preserving non-negatives’ order. As always, do not use built-in array functions.
 
 function removeNegatives(arr){
-    for (var i=0; i<arr.length; i++){
+    for (var i=arr.length-1; i>=0; i--){
         if(arr[i]<0){
             arr[i] = arr[i+1];
+            arr.length = arr.length-1;
+            console.log(arr);
         }
     }
-    arr.length = arr.length-1;
     return arr;
+
+    // for (var i=0; i<arr.length; i++){
+    //     if(arr[i]<0){
+    //         arr[i] = arr[i+1];
+    //     }
+    // }
+    // arr.length = arr.length-1;
+    // return arr;
 }
-console.log(removeNegatives([1,2,-3,-4,5]));
+console.log(removeNegatives([-1,2,-3,-4,5]));
