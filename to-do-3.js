946de2c1bc74df80a74599aa27e5f14ed4c380e8
Ricky <rickdhanota@gmail.com>
2020-08-23 19:06:27 -0700

    fixed it wooo

--- to-do-3.js
+++ to-do-3.js
@@ -1,9 +1,12 @@
 //Implement removeNegatives() that accepts an array, removes negative values, and returns the same array (not a copy), preserving non-negatives’ order. As always, do not use built-in array functions.
 
 function removeNegatives(arr){
-    for (var i=arr.length-1; i>=0; i--){
-        if(arr[i]<0){
-            arr[i] = arr[i+1];
+    for (var i=arr.length-1; i>=0; i--){//4,3, 2, 1, 0
+        if(arr[i]<0){//5, -4, -3, 2, -1
+            arr[i] = arr[i+1];//-4 = 5, -3 =5, 
+            if(arr[i+1]>0){
+                arr[i+1] = arr[i+2];
+            }
             arr.length = arr.length-1;
             console.log(arr);
         }
