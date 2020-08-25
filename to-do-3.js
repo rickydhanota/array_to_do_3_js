662f9cf9be86a6109a30243b4c2af9c46d195402
Ricky <rickdhanota@gmail.com>
2020-08-24 19:11:06 -0700

    fixed last function

--- to-do-3.js
+++ to-do-3.js
@@ -1,7 +1,5 @@
 //Implement removeNegatives() that accepts an array, removes negative values, and returns the same array (not a copy), preserving non-negatives’ order. As always, do not use built-in array functions.
 
-////// NEED HELP HERE!!!!!!!! ////////
-
 function removeNegatives(arr){
     for (var i=arr.length-1; i>=0; i--){
         if(arr[i]<0){
@@ -57,16 +55,17 @@ function fromArrayEnd(arr, val){
 //Lovely Burbank has a breathtaking view of the Los Angeles skyline. Let’s say you are given an array with heights of consecutive buildings, starting closest to you and extending away. Array [-1,7,3] would represent three buildings: first is actually out of view below street level, behind it is second at 7 stories high, third is 3 stories high (hidden behind the 7-story). You are situated at street level. Return array containing heights of buildings you can see, in order. Given [-1,1,1,7,3] return [1,7]. Given [0,4] return [4]. As always with challenges, do not use built-in array functions such as unshift().
 
 function buildingHeight(arr){
+    var max = 0;
     var newArr = [];
     for (var i=0; i<arr.length; i++){
         if(arr[i]<0){
-            console.log("Building is underground, I cant see it")
             continue
         }
-        else if(arr[i]>0 && arr[i]>arr[i-1]){
-            console.log(arr[i]);
+        else if (arr[i]>max){
+            max = arr[i];
+            newArr.push(max);
         }
     }
-    return
+    return newArr;
 }
-console.log(buildingHeight([1,-1,7,3]))
+console.log(buildingHeight([1,-1,7,3]))//3,1,7
