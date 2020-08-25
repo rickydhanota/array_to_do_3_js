a54528b7da3c3211914594b6405e2773bab288e8
Ricky <rickdhanota@gmail.com>
2020-08-24 18:08:14 -0700

    updated functions

--- to-do-3.js
+++ to-do-3.js
@@ -45,7 +45,7 @@ function fromArrayEnd(arr, val){
     // console.log(arr[arr.length-val]);
     return arr[arr.length-val];
 }
-console.log(fromArrayEnd([5,2,3,6,4,9,7], 3));
+// console.log(fromArrayEnd([5,2,3,6,4,9,7], 3));
 
 //Liam has "N" number of Green Belt stickers for excellent Python projects. Given arr and N, return the Nth-largest element, where (N-1) elements are larger. Return null if needed.
 
@@ -55,3 +55,18 @@ console.log(fromArrayEnd([5,2,3,6,4,9,7], 3));
 
 
 //Lovely Burbank has a breathtaking view of the Los Angeles skyline. Let’s say you are given an array with heights of consecutive buildings, starting closest to you and extending away. Array [-1,7,3] would represent three buildings: first is actually out of view below street level, behind it is second at 7 stories high, third is 3 stories high (hidden behind the 7-story). You are situated at street level. Return array containing heights of buildings you can see, in order. Given [-1,1,1,7,3] return [1,7]. Given [0,4] return [4]. As always with challenges, do not use built-in array functions such as unshift().
+
+function buildingHeight(arr){
+    var newArr = [];
+    for (var i=0; i<arr.length; i++){
+        if(arr[i]<0){
+            console.log("Building is underground, I cant see it")
+            continue
+        }
+        else if(arr[i]>0 && arr[i]>arr[i-1]){
+            console.log(arr[i]);
+        }
+    }
+    return
+}
+console.log(buildingHeight([1,-1,7,3]))
